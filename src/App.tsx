de04b541e719982cd2c04ebf85f48e4116d8a308
lovewei0 <xdw666888@qq.com>
2023-10-30 16:41:56 +0800

    feat: highSpeed component static

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,11 @@ import './App.css'
 import Header from './components/header'
 import Journey from './components/journey'
 import DepartDate from './components/departDate'
+// classnames
+import classnames from 'classnames'
 
 export default function App() {
+  const highSpeed = false
   return (
     <>
       <div className="header-wrapper">
@@ -15,6 +18,23 @@ export default function App() {
       <form action="./query.html" className="form">
         <Journey />
         <DepartDate />
+        <div className="high-speed">
+          <div className="high-speed-label">只看高铁/动车</div>
+          <div className="high-speed-switch">
+            <input type="hidden" name="highSpeed" />
+            <div
+              className={classnames('high-speed-track', {
+                checked: highSpeed
+              })}
+            >
+              <span
+                className={classnames('high-speed-handle', {
+                  checked: highSpeed
+                })}
+              ></span>
+            </div>
+          </div>
+        </div>
       </form>
     </>
   )
