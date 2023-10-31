7dc5029238a93e79f6d830b3a608f0e2468a90e0
lovewei0 <xdw666888@qq.com>
2023-10-31 18:50:29 +0800

    feat: config path

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { join } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': join(__dirname, 'src'),
+      '@pages': join(__dirname, 'src/pages'),
+      '@utils': join(__dirname, 'src/utils'),
+      '@store': join(__dirname, 'src/store')
+    }
+  },
   server: {
     proxy: {
       '/rest': {
